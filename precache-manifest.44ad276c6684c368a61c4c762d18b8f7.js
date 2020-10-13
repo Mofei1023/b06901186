9c19dc2e96821839f40f93300619cbda2186302f
Mofei1023 <71375565+Mofei1023@users.noreply.github.com>
2020-10-14 04:42:28 +0800

    Updates

---
+++ precache-manifest.44ad276c6684c368a61c4c762d18b8f7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289370ce1bc46727ab088777634ff5cb",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5e479073d4fdba16eaab",
+    "url": "/my-app/static/css/main.db7d6273.chunk.css"
+  },
+  {
+    "revision": "7a737f96ba7be86bbd89",
+    "url": "/my-app/static/js/2.32dceade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.32dceade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e479073d4fdba16eaab",
+    "url": "/my-app/static/js/main.d509f314.chunk.js"
+  },
+  {
+    "revision": "ce84308d645d40bc7e2a",
+    "url": "/my-app/static/js/runtime-main.b0842cb1.js"
+  },
+  {
+    "revision": "b6d609dc432d2fc28e42e1dd906f1acd",
+    "url": "/my-app/static/media/ethan.b6d609dc.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/my-app/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/my-app/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/my-app/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/my-app/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/my-app/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27e2aa9f10973de649ddef5c90196b5e",
+    "url": "/my-app/static/media/profile01.27e2aa9f.jpg"
+  },
+  {
+    "revision": "ba857ba2fa123e812b19aa152f1d1802",
+    "url": "/my-app/static/media/profile02.ba857ba2.jpg"
+  },
+  {
+    "revision": "bbb9af5c15fdddbd95b53597bf69fe3d",
+    "url": "/my-app/static/media/profile03.bbb9af5c.jpg"
+  },
+  {
+    "revision": "4209d83b19b05d70219c0d93fb8fc2f6",
+    "url": "/my-app/static/media/profile04.4209d83b.jpg"
+  }
+]);
